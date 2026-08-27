80a8c44ef234cefb455dc76bfde4e1e4a6797314
StivoMakMak <szbach704@gmail.com>
2026-08-27 14:23:02 -0300

    SFX added

--- Santa Mercê/project/Scripts/Menu/menu_hud.gd
+++ Santa Mercê/project/Scripts/Menu/menu_hud.gd
@@ -1,6 +1,8 @@
 extends CanvasGroup
 
 @onready var NGb = $NG # is a variable for get New Game button
+@onready var ESFX = $"../NewGame"
+@onready var SeFX = $"../Select"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,8 +11,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	
 	if NGb.button_pressed:
+		ESFX.play()
+		await get_tree().create_timer(1).timeout
 		get_tree().change_scene_to_file("res://Scenes/debug.tscn") #Simple Script for change scenes 
 	
 	pass
