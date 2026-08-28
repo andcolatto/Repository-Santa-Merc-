a2842a1f834471001c9b4084c7b4e76cd0a7335d
Anderson Colatto <andcolatto@gmail.com>
2026-08-28 18:31:12 -0300

    .

--- Santa Mercê/project/Scripts/Amanda/movement.gd
+++ Santa Mercê/project/Scripts/Amanda/movement.gd
@@ -4,14 +4,14 @@ extends CharacterBody2D
 @onready var IconZ = $CanvasGroup/Zicon
 
 func _ready() -> void:
-	# Altera a física para top-down (desliza em superfícies sem checar "chão")
-	motion_mode = MOTION_MODE_FLOATING
+	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 
 func _physics_process(_delta: float) -> void:
-	var input_direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_direction * speed
-	move_and_slide()
+	var input_vector: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	velocity = input_vector * speed
 
+	move_and_slide()
 
 
 #collision for buttons in hud --------------------------------
